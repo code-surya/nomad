9aae094db48a982592870e0e20dc037095091e20
code-surya <86817753+code-surya@users.noreply.github.com>
2025-12-26 09:19:58 +0530

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,6 +1,6 @@
 // Authentication utility functions
 
-const API_BASE_URL = 'https://nomad-production-e24e.up.railway.app;
+const API_BASE_URL = 'https://nomad-production-e24e.up.railway.app';
 
 // Store token in localStorage
 function setToken(token) {
